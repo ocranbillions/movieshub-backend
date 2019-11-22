ece2e4180af43dbc1358c937c668a7b49aaa3cf3
Samuel Ocran <sammiestt@gmail.com>
2019-11-22 17:25:35 +0100

    Initial application setup
    Install dependencies
    Configure db
    Run initial migrations
    Setup sample route
    Prepare initial heroku deployment

---
+++ config.py
@@ -0,0 +1,18 @@
+from flask_sqlalchemy import SQLAlchemy
+import os
+import json
+
+database_path = os.environ['DATABASE_URL']
+
+db = SQLAlchemy()
+
+'''
+setup_db(app)
+    binds a flask application and a SQLAlchemy service
+'''
+def setup_db(app, database_path=database_path):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
